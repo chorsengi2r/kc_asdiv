8b75d1e2a69455b35681091394b694c16c8168a9
chorsengi2r <71380382+chorsengi2r@users.noreply.github.com>
2023-06-06 23:29:54 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,23 +1,55 @@
 import torch
+import json
+import pandas as pd
 from transformers import BertTokenizer, BertForSequenceClassification
 from torch.utils.data import TensorDataset, DataLoader
 
+# Check if GPU is available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Prepare the test data
+print("Preparing test data...")
+# Opening JSON file
+f = open('asdiv_train_test.json')
+
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+test_data = data['test']
+
+test_texts = []
+test_labels = []
+for question in test_data:
+    test_texts.append(question['text'])
+    test_labels.append(question['label'])
+print("Text:", len(test_texts), "Labels:", len(test_labels))
+num_labels = pd.Series(test_labels).nunique()
+
+# Convert string labels to numerical labels
+label_map = {label: i for i, label in enumerate(set(test_labels))}
+numerical_labels = [label_map[label] for label in test_labels]
+
+
 # Load the pre-trained tokenizer and model
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=num_labels)
+
+# Move the model to the device
+model.to(device)
 
 # Load the trained model
-model.load_state_dict(torch.load('path/to/saved/model'))
-
-# Evaluate on the test data
-test_texts = ['Test sentence 1', 'Test sentence 2', ...]
-test_labels = [0, 1, ...]  # Assuming binary classification with 0 and 1 as labels
+model.load_state_dict(torch.load('./kc_model/'))
 
 # Tokenize the test texts
 encoded_test_inputs = tokenizer(test_texts, padding=True, truncation=True, return_tensors='pt')
 
+# Move the encoded test inputs and labels to the device
+input_ids = encoded_test_inputs['input_ids'].to(device)
+attention_mask = encoded_test_inputs['attention_mask'].to(device)
+labels = torch.tensor(numerical_labels).to(device)
+
 # Create a TensorDataset for the test data
-test_dataset = TensorDataset(encoded_test_inputs['input_ids'], encoded_test_inputs['attention_mask'], torch.tensor(test_labels))
+test_dataset = TensorDataset(input_ids, attention_mask, labels)
 
 # Create a DataLoader for the test data
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
